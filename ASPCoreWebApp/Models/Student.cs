21d6d7a0e5566d3d51691ba6f82850276faab8a6
ADMIN <ADMIN@DESKTOP-RNQISD0>
2024-08-02 18:10:50 +0530

    Add project files.

---
+++ ASPCoreWebApp/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPCoreWebApp.Models
+{
+    public class Student
+    {
+            public int studentId { get; set; }
+
+            [Required]
+            public string studentName { get; set; }
+            [Required]
+            public string gender { get; set; }
+            [Required]
+            public int age { get; set; }
+            [Required]
+            public string standard { get; set; }
+            [Required]
+            public DateTime dob { get; set; }
+      
+    }
+}
